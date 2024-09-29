0f8842ecf833fbf90b0255bc99bb2fb7ee81a4e3
MattWong-ca <matthew.yt.wong@gmail.com>
2024-09-29 18:57:24 -0400

    mint fxn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,12 +3,24 @@
 import Image from "next/image";
 import { useState } from "react";
 import { ethers } from "ethers";
+import nftJson from "./utils/nft.json";
+import { Alchemy, Network } from "alchemy-sdk";
 
 declare global {
   interface Window {
     ethereum?: import('ethers').Eip1193Provider
   }
 }
+
+const { API_KEY } = process.env;
+
+const settings = {
+  apiKey: API_KEY,
+  network: Network.SCROLL_SEPOLIA, // Replace with your desired network
+};
+
+const alchemy = new Alchemy(settings);
+
 
 export default function Home() {
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
@@ -30,10 +42,37 @@ export default function Home() {
     }
   };
 
+  async function mintNFT() {
+    if (!window.ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contractAddress = "0xfF70C3ae45022AE728b62c90d0c14D526560e9Cf"; // TO DO: get contract address from deployment
+      const mintContract = new ethers.Contract(contractAddress, nftJson.abi, signer);
+
+      const nonce = await alchemy.core.getTransactionCount(walletAddress!, "latest");
+      const gasEstimate = await mintContract.safeMint.estimateGas(walletAddress);
+
+      const tx = await mintContract.safeMint(walletAddress, 1, {
+        nonce: nonce,
+        gasLimit: gasEstimate,
+      })
+      await tx.wait();
+      console.log("Minted! ", tx.hash);
+    } catch (error) {
+      console.error("Error minting:", error);
+      alert("Failed to mint. Please try again.");
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center p-8 bg-[#fef1dd]">
       <div className="w-full flex justify-end mb-8">
-        <button 
+        <button
           onClick={connectWallet}
           className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
         >
@@ -52,7 +91,7 @@ export default function Home() {
           alt="NFT"
           style={{ border: '3px solid black', borderRadius: '10px', marginBottom: '24px' }}
         />
-        <button className="w-full bg-black text-white py-3 px-4 rounded font-semibold hover:bg-gray-800 transition-colors">
+        <button className="w-full bg-black text-white py-3 px-4 rounded font-semibold hover:bg-gray-800 transition-colors" onClick={mintNFT}>
           Mint
         </button>
       </div>
